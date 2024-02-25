25987583886735c17ce3b9278b4fe5ec173adc9c
MortadhaHouch <97769483+MortadhaHouch@users.noreply.github.com>
2024-02-25 18:51:12 +0100

    commit

--- database/database.js
+++ database/database.js
@@ -1,11 +1,27 @@
 let mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 let dotenv = require("dotenv");
 dotenv.config();
-let client;
-mongoose.connect(process.env.MONGODB_URI).then((client)=>{
-    client = client;
-    console.log("connected to DB");
-}).catch((error)=>{
-    console.log("error connecting");
-})
+const uri = process.env.MONGODB_URI;
+let client =new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+        await client.db("traveling-web-app").command({ ping: 1 });
+        mongoose.connect(uri).then((client)=>{
+            client = client
+            console.log("mongoose connected");
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        })
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
 module.exports = client;
